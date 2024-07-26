fc8bad7cc211ee98d258c9ea41d0502a6bb2a8df
[SW9] 형건우 <gudrjsdn8825@naver.com>
2024-07-26 21:55:28 +0900

    중복 파일 제거

--- middleware/async-handler.js
+++
@@ -1,9 +0,0 @@
-module.exports = (requestHandler) => {
-    return async (req, res, next) => {
-        try {
-            await requestHandler(req, res);
-        } catch(e) {
-            next(e);
-        }
-    }
-}
