7bb2cfca4b07bf6833cdb6df8e72585470dafe3e
[SW9] 형건우 <gudrjsdn8825@naver.com>
2024-07-29 16:21:43 +0900

    build 1차 테스트

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -11,6 +11,7 @@ const secret = process.env.COOKIE_SECRET;
 const setUserToken = (res, user) => {
     const token = jwt.sign(user, secret);
     res.cookie('token', token);
+    return token;
 };
 
 const router = Router();
@@ -20,14 +21,14 @@ const router = Router();
 router.post('/', loginCheck, passport.authenticate('local', {session: false}), (req, res, next) => {
     // 로그인 성공 했을 때 클라이언트에 줄 토큰에다가 signature(secret) 으로 서명 후 전달함.
     console.log(req.user);
-    setUserToken(res, req.user);
+    const token = setUserToken(res, req.user);
 
     // 관리자 계정 로그인 시 알림 팝업 확인
     if(req.user && req.user.is_admin){
         return res.status(200).json({code: 202, message: "관리자 로그인에 성공하였습니다."});
     }
     // 일반 로그인 성공 알림
-    return res.status(200).json({code: 200, message: "로그인에 성공하였습니다."});
+    return res.status(200).json({token: token, code: 200, message: "로그인에 성공하였습니다."});
 });
 
 module.exports = router;
